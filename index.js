c5c464bc25f4879eeccf3f9a69f6cb0d148c2d51
Utkarsh Gupta <utkarsh3333gupta@gmail.com>
2026-01-05 16:08:46 +0530

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// client.on(Events.ClientReady, (readyClient) => {
+//   console.log(`Logged in as ${readyClient.user.tag}!`);
+// });
+
+// client.on(Events.InteractionCreate, async (interaction) => {
+//   if (!interaction.isChatInputCommand()) return;
+
+//   if (interaction.commandName === "ping") {
+//     await interaction.reply("Pong!");
+//   }
+// });
+
+client.on("messageCreate", (message) => {
+  console.log(message.content);
+});
+
+client.login(TOKEN);
